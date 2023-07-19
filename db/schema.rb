68e6d998e4fa89c5a8e7dae1d2a75070de37990a
kei178 <keisuke.inaba@hey.com>
2023-07-19 11:18:36 +0700

    Add Message scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_19_040319) do
+ActiveRecord::Schema.define(version: 2023_07_19_041014) do
 
   create_table "legacy_users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
